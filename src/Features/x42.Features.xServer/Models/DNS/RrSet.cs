34caf5d24ab21d65cffb50aae8cfb92bab75b1fa
Dimitrios Savva <dimsavva@gmail.com>
2022-11-26 17:41:22 +0200

    Add DNS Endpoints

---
+++ src/Features/x42.Features.xServer/Models/DNS/RrSet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace x42.Features.xServer.Models.DNS
+{
+    public class RrSet
+    {
+        [JsonProperty("changetype")]
+        public string ChangeType { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("ttl")]
+        public int Ttl { get; set; }
+
+        [JsonProperty("records")]
+        public List<DnsRecord> Records { get; set; }
+    }
+}
